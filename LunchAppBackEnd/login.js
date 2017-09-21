ced16612f63f9818e7b4dc1a86e26e8510a4364e
Felix R <fidddz@gmail.com>
2017-09-21 08:09:18 +0200

    ;

--- LunchAppBackEnd/login.js
+++ LunchAppBackEnd/login.js
@@ -1,16 +1,27 @@
 function loginValidation(){
     var getUserName = document.loginForm.User.value;
     var getUserPassword = document.loginForm.Password.value;
-    var username = "user";
-    var password = "pass";
+    var username = "user"; //call to db : TODO
+    var password = "pass"; //call to db : TODO
     var counter = 0;
-    if(counter < 3){
-        if((getUserName == username) && (getUserPassword == password)){
-            return true;
-        }else{
-            alert("Something went wrong, try again")
-            counter++;
-            return false;
-        }
-    }
+
+    var content;
+    var fs = require("fs");
+    var file = "./TempDB.json";
+    fs.readFile(file, function read(err, data){
+        if(err) throw err;
+        content = data;
+        if(counter < 3){
+            if((data.name == username) && (data.password == password)){
+                return true;
+            }else{
+                alert("Something went wrong, try again")
+                counter++;
+                return false;
+            }
+        }  
+    });
+
+
+
 }
